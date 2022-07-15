b4a64141f93a66e52a6fa68424d4b6971a4a7621
YevhenVerbytskyi <109144032+YevhenVerbytskyi@users.noreply.github.com>
2022-07-15 15:31:28 +0300

    add Conditionalstatements&DatatypesPractical task

---
+++ Conditional statements and Data types Practical task.js
@@ -0,0 +1,56 @@
+// Conditional statements and Data types Practical task 
+
+// 1) Perform addition of various types (string + boolean, string + number, number + boolean)
+// string + boolean
+console.log("Yevhen " + false)
+
+// string + number
+console.log("Yevhen " + 5)
+
+// number + boolean
+console.log (1 + true)
+
+console.log("--------------------------------------------------")
+
+// 2) Perform multiplication of various types (string * boolean, string * number, number * boolean)
+// string * boolean
+console.log("Yevhen " * false)
+
+// string * number
+console.log("Yevhen " * 5)
+
+// number * boolean
+console.log (1 * true)
+
+console.log("--------------------------------------------------")
+
+// 3) Divide different types (string / boolean, string / number, number / Boolean)
+// string / boolean
+console.log("Yevhen " / false)
+
+// string / number
+console.log("Yevhen " / 5)
+
+// number / boolean
+console.log (1 / true)
+
+console.log("--------------------------------------------------")
+
+// 4) Perform explicit conversion (number, string, boolean)
+//number
+let str = "123";
+console.log(typeof str)
+let num = Number(str)
+console.log(typeof num)
+console.log("--------------------------------------------------")
+//string 
+let apple = true;
+console.log(typeof apple)
+apple = String(apple)
+console.log(typeof apple)
+console.log("--------------------------------------------------")
+//boolean
+let cherry = "red";
+console.log(typeof cherry)
+cherry = Boolean(cherry)
+console.log(typeof cherry)
